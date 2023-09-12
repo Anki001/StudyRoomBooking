4fd99f6ca04bb6f11f99db26b8aa63c2e3b5cf02
Ankush Gawande <ank.gawande@gmail.com>
2023-09-12 11:46:08 +0530

    FI-0001: Configuraton and setup of data access layer

---
+++ StudyRoomBooking.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyRoomBooking.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        
+        // Db set properties
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Code to enter default data
+        }
+    }
+}
---
+++ StudyRoomBooking.DataAccess/Configuration/DataAccessServiceInstaller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyRoomBooking.DataAccess.Configuration
+{
+    public static class DataAccessServiceInstaller
+    {
+        public static IServiceCollection RegisterDataAccessServiceDependencies(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            #region[In case of multitenant application, this can be move to Middleware or UnitOfWork]
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            #endregion
+
+            // Register dependency of StudyRoomBooking.DataAccess
+
+            return services;
+        }
+    }
+}
